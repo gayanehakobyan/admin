e2cd3de118bd255f8752e571ecf34f32a8428998
Gayane Hakobyan <gayanehakobyan93@gmail.com>
2021-05-14 16:17:21 +0400

    add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {auth} from "./firebase.js"
+
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState<any>(null)
+
+  React.useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      console.log(user);
+      setUser(user)
+    })
+  }, [])
+
+
+  const onclickHandler = () => {
+    console.log("mtav")
+    auth.createUserWithEmailAndPassword(username, password).then(cred => {
+      console.log("cred", cred)
+    })
+  }
+
+  const logoutHandler = () => {
+    auth.signOut().then(() => {
+      console.log("usrr sign out")
+    });
+  }
+
+  const logInHandler = () => {
+    console.log(username, password)
+    auth.signInWithEmailAndPassword(username, password).then(() => {
+      console.log("usrr sign in")
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input value ={username} onChange = {(e) => setUsername(e.currentTarget.value)}/>
+        <input value = {password} onChange = {(e) => setPassword(e.currentTarget.value)}/>
+        <button onClick = {onclickHandler}>
+            Submit
+        </button>
+
+        <button onClick = {logoutHandler}>
+            log out 
+        </button>
+
+        <button onClick = {logInHandler}>
+            log in 
+        </button>
     </div>
   );
 }
